8fd1457d31a486b058dd73038609501276737e53
jeevu94 <jeevan.jeevu94@gmail.com>
2022-10-10 13:13:47 +0530

    datase configured in base settings file

--- config/settings/base.py
+++ config/settings/base.py
@@ -42,7 +42,17 @@ LOCALE_PATHS = [str(ROOT_DIR / "locale")]
 # DATABASES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
-DATABASES = {"default": env.db("DATABASE_URL")}
+# DATABASES = {"default": env.db("DATABASE_URL")}
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "blogs",
+        "USER": "postgres",
+        "PASSWORD": "demo123",
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
+}
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 # https://docs.djangoproject.com/en/stable/ref/settings/#std:setting-DEFAULT_AUTO_FIELD
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
